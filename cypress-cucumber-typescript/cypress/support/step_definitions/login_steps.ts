b4feee5c9276b0fb15914fbbacbfea74137eda5f
Flora Pospisilova <flora.pospisilova@actumdigital.com>
2024-07-21 13:59:20 +0200

    Initial push

--- cypress-cucumber-typescript/cypress/support/step_definitions/login_steps.ts
+++ cypress-cucumber-typescript/cypress/support/step_definitions/login_steps.ts
@@ -1,14 +1,14 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
-import { loginPage } from '../../page-object/loginPage'
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { loginPage } from '../../page-object/loginPage';
 
 Given('the user is on the login page', () => {
-	loginPage.visit()
-})
+  loginPage.visit();
+});
 
 When('the user enters a valid username and password', () => {
-	loginPage.login()
-})
+  loginPage.login();
+});
 
 Then('the user should be redirected to the dashboard', () => {
-	cy.url().should('include', '/account')
-})
+  cy.url().should('include', '/account');
+});
